e20903bd08aa5809e535e21e6c16e21234048602
Vince Broz <vince@broz.cc>
2015-07-29 12:04:19 -0400

    Fix whitespace

--- ex8.js
+++ ex8.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
- // Write a program that performs an HTTP GET request to a URL provided
+// Write a program that performs an HTTP GET request to a URL provided
 // to you as the first command-line argument. Collect all data from
 // the server (not just the first "data" event) and then write two
 // lines to the console (stdout).
